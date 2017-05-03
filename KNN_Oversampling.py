78bc5bfb77fabbb02f5fd83a4ad283b9fb12ef86
Wiguna <wiguna.ahmad@yahoo.com>
2017-05-03 18:45:45 +0700

    Tambah menu

--- KNN_Oversampling.py
+++ KNN_Oversampling.py
@@ -1,173 +1,223 @@
 __author__ = 'ahmadauliawiguna'
-
-__author__ = 'ahmadauliawiguna'
+print 'Importing libraries.....'
+import os
+import subprocess as sp
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 import urllib
 import pprint
 import operator
-from sklearn.datasets import make_classification
 from sklearn.decomposition import PCA
 from sklearn.naive_bayes import GaussianNB
-from imblearn.under_sampling import RandomUnderSampler
+# from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import precision_score
 from sklearn.neighbors import NearestNeighbors
+import warnings
+warnings.filterwarnings("ignore")
 pp = pprint.PrettyPrinter(indent=4)
-
-#-------------------------------------------------------LOAD DATASET--------------------------------------------
-url = "http://localhost/dataset/abalone.data"
-# download the file
-raw_data = urllib.urlopen(url)
-# load the CSV file as a numpy matrix
-dataset = np.loadtxt(raw_data, delimiter=",")
-# separate the data from the target attributes
-X = dataset[:,0:dataset.shape[1]-2] #ambil kolom dari kolom ke 0 sampai ke kolom 2 dari kanan
-y = dataset[:,dataset.shape[1] - 1] #ambil kolom terakhir
-y_class,index_class,jumlah_class  = np.unique(y,return_counts=True, return_index=True) #dapatkan target labelnya apa aja
-min_index,min_class = min(enumerate(jumlah_class), key=operator.itemgetter(1)) #min_class jumlah class terkecil
-max_index,max_class = max(enumerate(jumlah_class), key=operator.itemgetter(1)) #max_class jumlah class terbesar
-
-#pisahkan dataset
-arrays = {}
-arrays_final = {}
-
-print 'Jumlah Class : ',jumlah_class
-print 'Jumlah Record di Class minoritas : ',min_class
-print 'Jumlah Record di Class mayoritas : ',max_class
-
-#looping class yang ada
-for target in y_class:
-    arrays[target] = []
-    arrays_final[target] = [] #menampung data asli/ori
-
-    for (index,target_label) in enumerate(y): #looping y, dapatkan target label dan recordnya
-        if target_label==target: #jika record = target
-            arrays[target].append(X[index])
-            arrays_final[target].append(X[index]) #menampung data asli/ori
-
-# looping array, cari yang jumlahnya kurang dari max_class
-for (index,target_label) in enumerate(arrays):
-    if len(arrays[target_label]) < max_class:
-        size_diambil = math.ceil(0.75 * len(arrays[target_label])) #jumlah record yang mau dioversamplingkan
-        size_class_sekarang = len(arrays[target_label]) #ukuran class sekarang
-
-        k = len(arrays[target_label])
-        nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(arrays[target_label])
-        #cari kedekatan antar record
-        distances, indices = nbrs.kneighbors(arrays[target_label])
-        index_tetangga = indices[0][::-1] #balik array, karena mengambil item yang paling tidak bertetangga sejumlah min_class
-        # index_tetangga = indices[0]
-        index_tetangga = index_tetangga[0:int(size_diambil)] #ambil index sejumlah size_diambil
+print 'Finish import.....'
 
 
-        print 'Size class ',target_label,' sebelum dioversampling : ',int(size_diambil)
+#BEGIN
+def print_menu() :
+    for file in os.listdir("/Applications/XAMPP/htdocs/dataset"):
+        if file.endswith(".data"):
+            datasets.append(file)
 
-        #kosongkan array
-        #arrays[target_label] = []
+    for (index_file,file) in enumerate(datasets):
+        print '[',index_file,'] ',file
 
-        #looping sampai jumlah class sekarang >= class maksimal
-        while len(arrays[target_label]) <= max_class:
-            for ambil in index_tetangga:
-                arrays[target_label].append(arrays_final[target_label][ambil]) #AMBIL DATA BERDASAR TARGET DAN INDEX PALING TIDAK BERTETANGA
+loop = True
+while loop :
+    datasets = []
+    print 100 * '-'
+    print_menu()
+    state  = True
+    chosen_file = ''
+    while state:
+        dataset = raw_input('Dataset [0-' + str(len(datasets)-1) +']: ')
+        if dataset.lower() != 'exit':
+            if (int(dataset) > (len(datasets)-1) or int(dataset) < 0):
+                state  = True
+            else:
+                chosen_file = datasets[int(dataset)]
+                state = False
+        else:
+            state = False
+            loop = False
+    if loop:
+        print 'Dataset : ' , chosen_file
 
-        #jika size class sekarang melebihi jumlah class maksimal, kurangi saja
-        arrays[target_label] = arrays[target_label][0:max_class]
-X_rknn = []
-y_rknn = []
-kolom = 0
-baris = 0
+        state = True
+        while state:
+            separator = raw_input('Pemisah kolom : ')
+            if separator != ',' and separator != ';' and separator.lower() != 'exit':
+                state  = True
+            else :
+                if separator.lower() == 'exit':
+                    loop = False
+                state = False
 
-print '-----------------------------------------------------------'
+    if loop:
+        clear = lambda : os.system('clear')
+        clear()
+        #-------------------------------------------------------LOAD DATASET--------------------------------------------
+        url = "http://localhost/dataset/" + chosen_file
+        # download the file
+        raw_data = urllib.urlopen(url)
+        # load the CSV file as a numpy matrix
+        dataset = np.loadtxt(raw_data, delimiter=separator)
+        # separate the data from the target attributes
+        X = dataset[:,0:dataset.shape[1]-2] #ambil kolom dari kolom ke 0 sampai ke kolom 2 dari kanan
+        y = dataset[:,dataset.shape[1] - 1] #ambil kolom terakhir
+        y_class,index_class,jumlah_class  = np.unique(y,return_counts=True, return_index=True) #dapatkan target labelnya apa aja
+        min_index,min_class = min(enumerate(jumlah_class), key=operator.itemgetter(1)) #min_class jumlah class terkecil
+        max_index,max_class = max(enumerate(jumlah_class), key=operator.itemgetter(1)) #max_class jumlah class terbesar
 
-for (index,target_label) in enumerate(arrays):
-    print 'Size class ',target_label,' sesudah dioversampling ',len(arrays[target_label])
-    for data in arrays[target_label]:
-        X_rknn.append(data)
-        y_rknn.append(target_label)
-X_rknn = np.array(X_rknn) #convert normal array to numpy array
-y_rknn = np.array(y_rknn) #convert normal array to numpy array
+        #pisahkan dataset
+        arrays = {}
+        arrays_final = {}
 
-print 'Size Sebelum Oversampling',X.shape
-print 'Size Sesudah Oversampling',X_rknn.shape
-print '-------------------------------------------------------BELUM PREPROCESSING-------------------'
-gaus_before = GaussianNB()
+        print 'Jumlah Class : ',jumlah_class
+        print 'Jumlah Record di Class minoritas : ',min_class
+        print 'Jumlah Record di Class mayoritas : ',max_class
 
-#kita split training dan testing 3 : 10
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=40)
-clf = gaus_before.fit(X_train,y_train)
+        #looping class yang ada
+        for target in y_class:
+            arrays[target] = []
+            arrays_final[target] = [] #menampung data asli/ori
 
-#scoring
-skor = gaus_before.score(X_test,y_test)
-print('Score tanpa preprocessing : ',skor)
-print('Sample Size tanpa preprocessing : ',X.shape)
-scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
-print("Accuracy tanpa preprocessing : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+            for (index,target_label) in enumerate(y): #looping y, dapatkan target label dan recordnya
+                if target_label==target: #jika record = target
+                    arrays[target].append(X[index])
+                    arrays_final[target].append(X[index]) #menampung data asli/ori
 
+        # looping array, cari yang jumlahnya kurang dari max_class
+        for (index,target_label) in enumerate(arrays):
+            if len(arrays[target_label]) < max_class:
+                size_diambil = math.ceil(0.75 * len(arrays[target_label])) #jumlah record yang mau dioversamplingkan
+                size_class_sekarang = len(arrays[target_label]) #ukuran class sekarang
 
-print '-------------------------------------------------------RANDOM OVERSAMPING-------------------'
-# Apply the random under-sampling
-rus = RandomOverSampler(random_state=40)
-X_resampled, y_resampled = rus.fit_sample(X, y)
-#declare Gaussian and fit with resampled dataset
-gaus = GaussianNB()
-X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=.3, random_state=40)
-clf = gaus.fit(X_train,y_train)
-
-#scoring
-skor = gaus.score(X_test,y_test)
-
-
-print('Score : ',skor)
-print('Data Testing Size : ',X_test.shape)
-print('Undersampling Size : ',X_resampled.shape)
-scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
-scores.shape
-print("Accuracy sesudah sampling : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
-
-
-#-------------------------------------------------------OVER SAMLPLING----------------------
-# Apply the random under-sampling
-rus = RandomOverSampler(random_state=40)
-X_resampled, y_resampled = rus.fit_sample(X, y)
-
-X_oversampling = X_resampled
-y_oversampling = y_resampled
+                k = len(arrays[target_label])
+                nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(arrays[target_label])
+                #cari kedekatan antar record
+                distances, indices = nbrs.kneighbors(arrays[target_label])
+                index_tetangga = indices[0][::-1] #balik array, karena mengambil item yang paling tidak bertetangga sejumlah min_class
+                # index_tetangga = indices[0]
+                index_tetangga = index_tetangga[0:int(size_diambil)] #ambil index sejumlah size_diambil
 
 
-print '-------------------------------------------------------kRNN OVERSAMPLING-------------------'
-#-------------------------------------------------------kRNN UNDER SAMLPLING----------------------
-gaus_before = GaussianNB()
+                print 'Size class ',target_label,' sebelum dioversampling : ',int(size_diambil)
 
-#kita split training dan testing 3 : 10
-X_train, X_test, y_train, y_test = train_test_split(X_rknn, y_rknn, test_size=.3, random_state=40)
-clf = gaus_before.fit(X_train,y_train)
+                #kosongkan array
+                #arrays[target_label] = []
 
-#scoring
-skor = gaus_before.score(X_test,y_test)
-print('Score : ',skor)
-print('kRNN Size : ',X_rknn.shape)
-print('Data Testing Size : ',X_test.shape)
-scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
-print("Accuracy kRNN : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+                #looping sampai jumlah class sekarang >= class maksimal
+                while len(arrays[target_label]) <= max_class:
+                    for ambil in index_tetangga:
+                        arrays[target_label].append(arrays_final[target_label][ambil]) #AMBIL DATA BERDASAR TARGET DAN INDEX PALING TIDAK BERTETANGA
 
-print '-------------------------------------------------------kRNN RANDOM OVERSAMPLING-------------------'
-#-------------------------------------------------------kRNN UNDER SAMLPLING RANDOM----------------------
-gaus_before = GaussianNB()
+                #jika size class sekarang melebihi jumlah class maksimal, kurangi saja
+                arrays[target_label] = arrays[target_label][0:max_class]
+        X_rknn = []
+        y_rknn = []
+        kolom = 0
+        baris = 0
 
-#kita split training dan testing 3 : 10
-X_rknn = np.concatenate((X_rknn , X_oversampling))
-y_rknn = np.concatenate((y_rknn , y_oversampling))
-X_train, X_test, y_train, y_test = train_test_split(X_rknn, y_rknn, test_size=.3, random_state=40)
-clf = gaus_before.fit(X_train,y_train)
+        print 100*'-'
 
-#scoring
-skor = gaus_before.score(X_test,y_test)
-print('Score sebelum sampling : ',skor)
-print('kRNN Size : ',X_rknn.shape)
-print('Data Testing Size : ',X_test.shape)
-scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
-print("Accuracy kRNN Undersampling random Oversampling : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+        for (index,target_label) in enumerate(arrays):
+            print 'Size class ',target_label,' sesudah dioversampling ',len(arrays[target_label])
+            for data in arrays[target_label]:
+                X_rknn.append(data)
+                y_rknn.append(target_label)
+        X_rknn = np.array(X_rknn) #convert normal array to numpy array
+        y_rknn = np.array(y_rknn) #convert normal array to numpy array
+
+        print 'Size Sebelum Oversampling',X.shape
+        print 'Size Sesudah Oversampling',X_rknn.shape
+
+        #-------------------------------------------------------TANPA OVER SAMLPLING----------------------
+        print 35*'-','BELUM OVERSAMPLING',35*'-'
+        gaus_before = GaussianNB()
+
+        #kita split training dan testing 3 : 10
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.3, random_state=40)
+        clf = gaus_before.fit(X_train,y_train)
+
+        #scoring
+        skor = gaus_before.score(X_test,y_test)
+        print 'Score tanpa preprocessing : ',skor
+        print 'Sample Size tanpa preprocessing : ',X.shape
+        scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
+        print "Accuracy tanpa preprocessing : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+
+        #-------------------------------------------------------RANDOM OVER SAMLPLING----------------------
+        print 35*'-','RANDOM OVERSAMPING',35*'-'
+        # Apply the random under-sampling
+        rus = RandomOverSampler(random_state=40)
+        X_resampled, y_resampled = rus.fit_sample(X, y)
+        #declare Gaussian and fit with resampled dataset
+        gaus = GaussianNB()
+        X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=.3, random_state=40)
+        clf = gaus.fit(X_train,y_train)
+
+        #scoring
+        skor = gaus.score(X_test,y_test)
+
+
+        print 'Score : ',skor
+        print 'Data Testing Size : ',X_test.shape
+        print 'Undersampling Size : ',X_resampled.shape
+        scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
+        scores.shape
+        print "Accuracy sesudah sampling : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+
+        # Apply the random under-sampling
+        rus = RandomOverSampler(random_state=40)
+        X_resampled, y_resampled = rus.fit_sample(X, y)
+
+        X_oversampling = X_resampled
+        y_oversampling = y_resampled
+
+
+        #-------------------------------------------------------kRNN OVER SAMLPLING----------------------
+        print 35*'-','kRNN OVERSAMPLING',35*'-'
+        gaus_before = GaussianNB()
+
+        #kita split training dan testing 3 : 10
+        X_train, X_test, y_train, y_test = train_test_split(X_rknn, y_rknn, test_size=.3, random_state=40)
+        clf = gaus_before.fit(X_train,y_train)
+
+        #scoring
+        skor = gaus_before.score(X_test,y_test)
+        print 'Score : ',skor
+        print 'kRNN Size : ',X_rknn.shape
+        print 'Data Testing Size : ',X_test.shape
+        scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
+        print "Accuracy kRNN : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
+
+
+        #-------------------------------------------------------kRNN UNDER SAMLPLING RANDOM----------------------
+        print 35*'-','kRNN RANDOM OVERSAMPLING',35*'-'
+        gaus_before = GaussianNB()
+
+        #kita split training dan testing 3 : 10
+        X_rknn = np.concatenate((X_rknn , X_oversampling))
+        y_rknn = np.concatenate((y_rknn , y_oversampling))
+        X_train, X_test, y_train, y_test = train_test_split(X_rknn, y_rknn, test_size=.3, random_state=40)
+        clf = gaus_before.fit(X_train,y_train)
+
+        #scoring
+        skor = gaus_before.score(X_test,y_test)
+        print 'Score sebelum sampling : ',skor
+        print 'kRNN Size : ',X_rknn.shape
+        print 'Data Testing Size : ',X_test.shape
+        scores = cross_val_score(clf , X_test,y_test, cv=3) #10fold cross validation
+        print "Accuracy kRNN Undersampling random Oversampling : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)
